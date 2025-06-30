65041b9ac822036a796e9d57284b1a61b0cfb688
daljeet mathur <daljeet.mathur2002@gmail.com>
2025-06-30 21:52:55 +0530

    add component homepage

---
+++ frontend/src/landing_page/OpenAccount.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function OpenAccount() {
+    return ( <h1>OpenAccount Section</h1> );
+}
+
+export default OpenAccount;
---
+++ frontend/src/landing_page/home/Trust.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function Trust() {
+    return ( <h1>Trust Section</h1> );
+}
+
+export default Trust;
