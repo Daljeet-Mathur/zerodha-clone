22743db47e4dde244325e23a2c8bb44d1e942a8d
daljeet mathur <daljeet.mathur2002@gmail.com>
2025-07-01 22:53:28 +0530

    add navbar and footer and trust section

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import Signup from './landing_page/signup/Signup';
+import About from './landing_page/about/About';
+import Products from './landing_page/product/Products';
+import Pricing from './landing_page/home/Pricing';
+import Support from './landing_page/support/Support';
+import Navbar from './landing_page/Navbar';
+import Footer from './landing_page/Footer';
 import HomePage from './landing_page/home/HomePage';
 
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <HomePage />
+    <BrowserRouter>
+     <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Products" element={<Products />} />
+        <Route path="/Pricing" element={<Pricing />} />
+        <Route path="/Support" element={<Support />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
