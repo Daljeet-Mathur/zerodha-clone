50781ebb090635a6c98e78a8e3adf3b9956447dc
daljeet mathur <daljeet.mathur2002@gmail.com>
2025-07-09 20:52:46 +0530

    home page done

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container">
-                    <Link className="navbar-brand" to="/"><img src='\images\images\logo.svg' style={{width:"22%"}}/></Link>
+                    <Link className="navbar-brand col-3" to="/"><img src='\images\images\logo.svg' style={{width:"50%"}}/></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- frontend/src/landing_page/pricing/Pricing.js
+++ frontend/src/landing_page/pricing/Pricing.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 
 function Pricing() {
-    return (  <h1>Pricing</h1>);
+    return (
+        <>
+          <h1>section</h1>
+        </>
+    );
 }
 
 export default Pricing;
