58e5d481569715617996b7a278afe3ccc9dcc082
daljeet mathur <daljeet.mathur2002@gmail.com>
2025-08-06 20:18:58 +0530

    Added dashboard folder

---
+++ dashboard/src/components/Orders.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+// import { Link } from "react-router-dom";
+
+const Orders = () => {
+
+   const [addOders, setAddOders] = useState([]);
+     
+       useEffect(() => {
+         axios.get("http://localhost:3002/newOrder").then((res) => {
+          //  console.log(res.data);
+           setAddOders(res.data);
+         });
+       }, []);
+
+  return (
+    <div className="orders">
+      <h3 className="title">Oders ({addOders.length})</h3>
+
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Qty.</th>
+            <th>price.</th>
+            <th>mode</th>
+          </tr>
+
+          {addOders.map((stock, index) => {
+            return (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.price}</td>
+                <td>{stock.mode}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
